8df40cec5008b31825bbc877d3243b0f940b0af1
hashemsh <sh,sheikhypour@gmail.com>
2020-05-07 12:45:33 +0430

    make file for generat sitemap

---
+++ sitemap.php
@@ -0,0 +1,18 @@
+ndle = fopen("https://ezzatkhah.com/list-url.txt", "r");
+if ($handle) {
+while (($line = fgets($handle)) !== false) {
+?>
+<url>
+	<loc><?php echo $line ?></loc>
+	<priority>1</priority>
+	<changefreq>daily</changefreq>
+	<lastmod><?php echo date("c", $now); ?></lastmod>
+</url>
+<?php
+	}                                       
+fclose($handle);
+} 
+else {
+	echo "Error accessing Tracking Data";
+} 
+?>
